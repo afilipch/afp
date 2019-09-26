685b94cf5412c98f2f0df68af55d4884d31055ff
Filipchyk <filipchyk@ibt077.ibt.kfa-juelich.de>
2019-09-26 18:39:23 +0200

    fixed hrra table annotation

--- parsing/add_tss_to_cds.py
+++ parsing/add_tss_to_cds.py
@@ -21,35 +21,59 @@ parser.add_argument('--format', nargs = '?', default='png', type = str, help = "
 parser.add_argument('--outdir', nargs = '?', required=True, type = str, help = "Path to the output directory");
 args = parser.parse_args();
 
+def find_closest(tss, starts_plus, stops_minus):
+    score = float(tss[8].split("_")[-1])
+    if(tss.strand == '+'):
+        distances = [(x[0], x[1]-tss.start) for x in starts_plus]
+        distances = [x for x in distances if x[1]>-1]
+        if(not distances):
+            return None
+        cds, mind = min(distances, key=lambda x: x[1])
+        upstream = tss.start;
+        
+    if(tss.strand == '-'):
+        distances = [(x[0], tss.start-x[1]+1) for x in stops_minus]
+        distances = [x for x in distances if x[1]>-1]
+        if(not distances):
+            return None
+
+        cds, mind = min(distances, key=lambda x: x[1])
+        #if(mind==0):
+            #print(mind)
+        
+        upstream = tss.start+1
+        
+    return cds, upstream, score, mind
+
+
+
 cds_list = BedTool(args.cds)
 tss_list = BedTool([x for x in BedTool(args.tss) if x[2] == 'TSS'])
 OFFSET = 9;
 
-def parse_closest(interval, offset):
-    annotation = interval[offset+8]
-    score = float(interval[8].split("_")[-1])
-    if(annotation == '.'):
-        return [None, interval.start, interval.strand, score, 0]
-    else:
-        name = dict([x.split("=") for x in annotation.split(";")])['ID']
-        cds_start = int(interval[OFFSET+3])
-        cds_stop = int(interval[OFFSET+4])
-        if(interval.strand == '-'):
-            distance = interval.stop - cds_stop;
-        else:
-            distance = cds_start - interval.stop
-        return name, interval.start, interval.strand, score, distance
-    
-    
-
+starts_plus = [ (x.name, x.start) for x in cds_list if x.strand == '+']
+stops_minus = [ (x.name, x.stop) for x in cds_list if x.strand == '-']
 annotated_tss = [];
-for interval in tss_list.closest(b=cds_list, s = True, iu = True, D='a'):
-    annotated_tss.append(parse_closest(interval, OFFSET))
 
-orphans = [x for x in annotated_tss if not x[0]]
-orphans_distance = [x for x in annotated_tss if x[0] and x[4] > args.distance]
-orphans_inside = [x for x in annotated_tss if x[0] and x[4] < 0]
-valid_tss = [x for x in annotated_tss if x[0] and x[4] <= args.distance and x[4]>=0]
+#orphans = 0
+for tss in tss_list:
+    res = find_closest(tss, starts_plus, stops_minus)
+    if(res):
+        annotated_tss.append(res)
+        #if(res[3] > args.distance):
+            #print(tss, res[0])
+            #print("#"*160)
+    #else:
+        #print("NU")
+        
+#sys.exit()
+
+        
+        
+valid_tss = [x for x in annotated_tss if x[3] <= args.distance]
+orphans_distance = [x for x in annotated_tss if x[3] > args.distance]
+sys.stderr.write("\nTotal tss: %d\nValid tss: %d\n"  % tuple([len(x) for x in [tss_list, valid_tss]  ]) )
+
 
 geneid2tss = defaultdict(list);
 for vtss in valid_tss:
@@ -63,15 +87,15 @@ for cds in cds_list:
             if(cds.strand == '+'):
                 cds.start = tss;
             else:
-                cds.stop = tss+1;
+                cds.stop = tss;
             sys.stdout.write(str(cds))
     else:
         cds.attrs['tss_variants'] = '1'
         sys.stdout.write(str(cds))
-        
-    
-    
-sys.stderr.write("\nOrphans tss: %d\nLong distance tss: %d\nInside gene tss: %d\nValid tss: %d\n"  % tuple([len(x) for x in [orphans, orphans_distance, orphans_inside, valid_tss]  ]) )
+
+
+
+
 sys.stderr.write("\nnum_tss_per_cds\tnum_of_cds\n")
 tss_per_gene = list(sorted(Counter([len(x) for x in geneid2tss.values()]).items(), key = lambda x: x[0]))
 for kv in tss_per_gene:
@@ -81,8 +105,8 @@ for kv in tss_per_gene:
     
 ###############################################################################################################    
 ###Plotting
-step = 1000
-distances = [x[4] for x in valid_tss + orphans_distance]
+step = 5000
+distances = [x[3] for x in valid_tss + orphans_distance]
 distances = [x if x<step else step for x in distances]
 xvals, yvals = CDF(distances, zerovalue=0)
 
@@ -101,6 +125,94 @@ for axis in ['bottom','left','right']:
 ax.plot(xvals, yvals, color = 'darkblue', linewidth=linewidth)
 plt.savefig(os.path.join(args.outdir, "utr5_length.%s"  %  args.format) , format = args.format)
 #plt.show()
+
+
+
+
+
+
+
+
+
+
+#sys.exit()
+
+#def parse_closest(interval, offset):
+    #annotation = interval[offset+8]
+    #score = float(interval[8].split("_")[-1])
+    #if(annotation == '.'):
+        #return [None, interval.start, interval.strand, score, 0]
+    #else:
+        #name = dict([x.split("=") for x in annotation.split(";")])['ID']
+        #cds_start = int(interval[OFFSET+3])
+        #cds_stop = int(interval[OFFSET+4])
+        #if(interval.strand == '-'):
+            #distance = interval.stop - cds_stop;
+        #else:
+            #distance = cds_start - interval.stop
+        #return name, interval.start, interval.strand, score, distance
+    
+    
+
+
+#for interval in tss_list.closest(b=cds_list, s = True, iu = True, D='a'):
+    #annotated_tss.append(parse_closest(interval, OFFSET))
+
+#orphans = [x for x in annotated_tss if not x[0]]
+#orphans_distance = [x for x in annotated_tss if x[0] and x[4] > args.distance]
+#orphans_inside = [x for x in annotated_tss if x[0] and x[4] < 0]
+#valid_tss = [x for x in annotated_tss if x[0] and x[4] <= args.distance and x[4]>=0]
+
+#geneid2tss = defaultdict(list);
+#for vtss in valid_tss:
+    #geneid2tss[vtss[0]].append(vtss[1]);
+    
+#for cds in cds_list:
+    #tss_vars = geneid2tss.get(cds.name)
+    #if(tss_vars):
+        #cds.attrs['tss_variants'] = str(len(tss_vars)) 
+        #for tss in tss_vars:
+            #if(cds.strand == '+'):
+                #cds.start = tss;
+            #else:
+                #cds.stop = tss+1;
+            #sys.stdout.write(str(cds))
+    #else:
+        #cds.attrs['tss_variants'] = '1'
+        #sys.stdout.write(str(cds))
+        
+    
+    
+#sys.stderr.write("\nOrphans tss: %d\nLong distance tss: %d\nInside gene tss: %d\nValid tss: %d\n"  % tuple([len(x) for x in [orphans, orphans_distance, orphans_inside, valid_tss]  ]) )
+#sys.stderr.write("\nnum_tss_per_cds\tnum_of_cds\n")
+#tss_per_gene = list(sorted(Counter([len(x) for x in geneid2tss.values()]).items(), key = lambda x: x[0]))
+#for kv in tss_per_gene:
+    #sys.stderr.write("%d\t%d\n" % kv)
+    
+    
+    
+################################################################################################################    
+####Plotting
+#step = 1000
+#distances = [x[4] for x in valid_tss + orphans_distance]
+#distances = [x if x<step else step for x in distances]
+#xvals, yvals = CDF(distances, zerovalue=0)
+
+#fontsize, linewidth = 28, 5
+
+#fig, ax = plt.subplots(figsize=(16,9))
+#plt.tight_layout(rect=[0.1, 0.1, 0.9, 0.9])
+#ax.set_xlabel('5\'UTR length', fontsize=fontsize)
+#ax.set_ylabel('CDF', fontsize=fontsize)
+#ax.tick_params(axis='both', labelsize=fontsize, top=False, right=False)
+#ax.spines['top'].set_visible(False)
+#ax.spines['right'].set_visible(False)
+#for axis in ['bottom','left','right']:
+    #ax.spines[axis].set_linewidth(linewidth)
+
+#ax.plot(xvals, yvals, color = 'darkblue', linewidth=linewidth)
+#plt.savefig(os.path.join(args.outdir, "utr5_length.%s"  %  args.format) , format = args.format)
+##plt.show()
 
     
     
--- postanalyses/reannotate_according_tss.py
+++ postanalyses/reannotate_according_tss.py
@@ -15,71 +15,144 @@ from afbio.pybedtools_af import construct_gff_interval
 parser = argparse.ArgumentParser(description='Annotates the provided genomic regions with the distances to the closest transcription start sites');
 parser.add_argument('path', metavar = 'N', nargs = '?', type = str, help = "Path to the genomic regions, gff format (output of 'postanalyses/annotate_tss.py')");
 parser.add_argument('--cds', nargs = '?', required=True, type = str, help = "Path to the cds regions, gff format");
+parser.add_argument('--transcripts', nargs = '?', required=True, type = str, help = "Path to the transcripts regions, gff format");
 parser.add_argument('--inside', nargs = '?', default=50, type = int, help = "Maximum allowed distance to TSS while inside a gene");
 parser.add_argument('--maxd', nargs = '?', default=700, type = int, help = "Maximum allowed distance to TSS");
-
-parser.add_argument('--annotation', nargs = '?',  type = str, help = "If set, the intervals will be renamed and annotated");
-
+parser.add_argument('--annotation', nargs = '?', required = True, type = str, help = "If set, the intervals will be renamed and annotated");
 args = parser.parse_args();
 
-name2strand = {"tss_downstream_minus_distance": "-", "tss_downstream_plus_distance": "+", "tss_upstream_minus_distance": "-", "tss_upstream_plus_distance": "+"}
-
-def select_closest(interval, cds_dict, inside, maxd):
-    outside_gene = "tss_downstream_minus_distance", "tss_downstream_plus_distance"
-    inside_gene ="tss_upstream_minus_distance", "tss_upstream_plus_distance";
-    distances = [(x,int(interval.attrs[x])) for x in outside_gene];
-    
-    for name in inside_gene:
-        distance = int(interval.attrs[name])
-        if(distance<=inside):
-            distances.append((name, distance))
-            
-    min_name, mindistance = min(distances, key=lambda x: x[1])
-    gene = interval.attrs[min_name.replace("distance", "gene")]
-    strand = name2strand[min_name]
-    cds = cds_dict.get(gene)
-    if(cds and mindistance<=maxd):
+def find_closest(peak, starts_plus, stops_minus, cds_dict, ann_dict, d_threshold, inside):
+    center = int(peak.name)
+    distances = [('+', x[0], x[1]-center) for x in starts_plus]
+    distances.extend([('-', x[0], center-x[1]+1) for x in stops_minus]);
+    distances = [x for x in distances if x[2]>-1*inside]
+    strand, gene_name, mindistance = min(distances, key = lambda x: abs(x[2]))
+    if(abs(mindistance) <= d_threshold):
+        gene_name = "_".join(gene_name.split("_")[:-1])
+        cds = cds_dict[gene_name]
         if(strand == '+'):
-            atg = cds.start - (interval.start + interval.stop)//2
+            atg = cds.start - center
+        if(strand == '-'):
+            atg = center - cds.end + 1
+    
+        gene_name = gene_name.split("-")[1]
+        ann = ann_dict.get(gene_name);
+        if(ann):
+            genesymbol = ann[0]
+            annotation = ann[1]
+            function = ann[2]
         else:
-            atg = (interval.start + interval.stop)//2 - cds.stop
+            genesymbol = gene_name
+            annotation = 'Unknown'
+            function = "Unknown"
+        
+        
+        return construct_gff_interval(peak.chrom, peak.start, peak.stop, 'consensus_region', score='0', strand=strand, source='un', frame='.', attrs=[("Name", peak.attrs['Name']), ("maxcov", peak.attrs['maxcov']), ("zscores", peak.attrs['zscores']), ("peakpos", peak.attrs['peakpos']), ("annotation", annotation), ("function", function), ("gene", gene_name), ("genesymbol", genesymbol), ("tss", mindistance), ("atg", abs(atg))])
+    else:
+        #print(peak, strand, gene_name, mindistance)
+        #print("#"*150)
+        return None
+
+    
+
+
+ann_dict = {}
+with open(args.annotation) as f:
+    next(f)
+    for l in f:
+        a = l.strip().split(";")
+        if(len(a) > 9 and a[1] and a[2]):
+            ann_dict[a[1]] = a[2], a[8], a[9]
+            
+            
+#print(ann_dict)
+
+#sys.exit()
+
+cds_dict = dict([ (x.name, x) for x in BedTool(args.cds) ])
+tr_list = BedTool(args.transcripts)
+starts_plus = [ (x.name, x.start) for x in tr_list if x.strand == '+']
+stops_minus = [ (x.name, x.stop) for x in tr_list if x.strand == '-']
+
+
+
+peaks = BedTool(args.path);
+filtered_out = 0;
+for peak in peaks:
+    newpeak = find_closest(peak, starts_plus, stops_minus, cds_dict, ann_dict, args.maxd, args.inside)
+    if(newpeak):
+        sys.stdout.write(str(newpeak))
+        pass;
+    else:
+        filtered_out += 1;
+        
+sys.stderr.write("\nTotal peaks: %d\nPeaks passed distance threshold: %d\n"  % (len(peaks), len(peaks) - filtered_out) )
+    
+
+
+
+
+
+
+
+#name2strand = {"tss_downstream_minus_distance": "-", "tss_downstream_plus_distance": "+", "tss_upstream_minus_distance": "-", "tss_upstream_plus_distance": "+"}
+
+#def select_closest(interval, cds_dict, inside, maxd):
+    #outside_gene = "tss_downstream_minus_distance", "tss_downstream_plus_distance"
+    #inside_gene ="tss_upstream_minus_distance", "tss_upstream_plus_distance";
+    #distances = [(x,int(interval.attrs[x])) for x in outside_gene];
+    
+    #for name in inside_gene:
+        #distance = int(interval.attrs[name])
+        #if(distance<=inside):
+            #distances.append((name, distance))
+            
+    #min_name, mindistance = min(distances, key=lambda x: x[1])
+    #gene = interval.attrs[min_name.replace("distance", "gene")]
+    #strand = name2strand[min_name]
+    #cds = cds_dict.get(gene)
+    #if(cds and mindistance<=maxd):
+        #if(strand == '+'):
+            #atg = cds.start - (interval.start + interval.stop)//2
+        #else:
+            #atg = (interval.start + interval.stop)//2 - cds.stop
 
      
-        res = construct_gff_interval(interval.chrom, interval.start, interval.stop, 'consensus_region', score='0', strand=strand, source='un', frame='.', attrs=[("Name", interval.attrs['Name']), ("maxcov", interval.attrs['maxcov']), ("zscores", interval.attrs['zscores']), ("peakpos", interval.attrs['peakpos']), ("gene", gene), ("tss", mindistance), ("atg", abs(atg)), ("annotation", mindistance) ])
-        return res
-    else:
-        return None
+        #res = construct_gff_interval(interval.chrom, interval.start, interval.stop, 'consensus_region', score='0', strand=strand, source='un', frame='.', attrs=[("Name", interval.attrs['Name']), ("maxcov", interval.attrs['maxcov']), ("zscores", interval.attrs['zscores']), ("peakpos", interval.attrs['peakpos']), ("gene", gene), ("tss", mindistance), ("atg", abs(atg)), ("annotation", mindistance) ])
+        #return res
+    #else:
+        #return None
         
         
     
-cds_dict = dict([ (x.name.split("-")[1], x) for x in BedTool(args.cds) ])
-if(args.annotation):
-    ann_dict = dict([ (x.attrs['geneid'].replace("cg", "NCgl"), x) for x in BedTool(args.annotation) ])
+#cds_dict = dict([ (x.name.split("-")[1], x) for x in BedTool(args.cds) ])
+#if(args.annotation):
+    #ann_dict = dict([ (x.attrs['geneid'].replace("cg", "NCgl"), x) for x in BedTool(args.annotation) ])
     
     
 
 
     
-for interval in BedTool(args.path):
-    res = select_closest(interval, cds_dict, args.inside, args.maxd)
-    if(res):
-        if(args.annotation):
-            #print(res.attrs['gene'])
-            ann = ann_dict.get(res.attrs['gene']);
-            if(ann):
-                genesymbol = ann.name;
-                annotation = ann.attrs['annotation']
-                function = ann.attrs['function']
-            else:
-                genesymbol = res.attrs['gene'].replace("NCgl", "cg")
-                annotation = 'Unknown'
-                function = "Unknown"
-            res.attrs['genesymbol'] = genesymbol
-            res.attrs['annotation'] = annotation
-            res.attrs['function'] = function
+#for interval in BedTool(args.path):
+    #res = select_closest(interval, cds_dict, args.inside, args.maxd)
+    #if(res):
+        #if(args.annotation):
+            ##print(res.attrs['gene'])
+            #ann = ann_dict.get(res.attrs['gene']);
+            #if(ann):
+                #genesymbol = ann.name;
+                #annotation = ann.attrs['annotation']
+                #function = ann.attrs['function']
+            #else:
+                #genesymbol = res.attrs['gene'].replace("NCgl", "cg")
+                #annotation = 'Unknown'
+                #function = "Unknown"
+            #res.attrs['genesymbol'] = genesymbol
+            #res.attrs['annotation'] = annotation
+            #res.attrs['function'] = function
             
                 
-        sys.stdout.write(str(res))
+        #sys.stdout.write(str(res))
     
     
     
