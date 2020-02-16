c7f710af79e06b6f9a656328d4719838d2efb416
afilipch <philipchick@gmail.com>
2020-02-16 19:01:39 +0300

    work on tss

--- genomic/tss/select_differential_tss.py
+++ genomic/tss/select_differential_tss.py
@@ -21,26 +21,46 @@ parser.add_argument('path', metavar = 'N', nargs = '?', type = str, help = "Path
 parser.add_argument('--transcripts', nargs = '?', required=True, type = str, help = "Path to the transcripts, gff format");
 parser.add_argument('--minexpr', nargs = '?', default = 100, type = float,  help = "Minimum allowed expression (TPM)");
 parser.add_argument('--mindiff', nargs = '?', default = 0.25, type = float,  help = "Minimum allowed difference in TSS fractions (normalized to 1)");
-#parser.add_argument('--outdir', required = True, nargs = '?', type = str, help = "Path to the output plot folder");
+parser.add_argument('--minfraction', nargs = '?', default = 2, type = float,  help = "Minimum allowed fraction to assign a spurious transcription");
+parser.add_argument('--outdir', required = True, nargs = '?', type = str, help = "Path to the output folder");
 #parser.add_argument('--format', default = 'png', nargs = '?', type = str, help = "Plot format");
 args = parser.parse_args()
 
+def get_score(e1, e2, diff):
+    return (min(e1) + min(e2))*diff
 
-def check(labels, var_names, expression, variants , minexpr, mindiff):
+
+#def check_spurious_transcription(var, minfraction):
+    #if(min(var) > minfraction)
+    
+    
+
+def check(labels, var_names, expression, variants , minexpr, mindiff, minfraction):
     expr_passed = [];
     for c1, c2 in combinations(range(len(expression)), 2):
         if(min(expression[c1])>=minexpr and min(expression[c2])>=minexpr):
             expr_passed.append((c1,c2))
-    result = [];
+            
+    normal = [];
+    spurious = [];
     for c1, c2 in expr_passed:
         for v in range(len(variants)):
             var1, var2 = variants[v][c1], variants[v][c2]
+            if(min(var1) > minfraction):
+                score = min(var1)*min(expression[c1])
+                spurious.append(( labels[c1], var_names[v], expression[c1], var1, score))
+            
             diff = max(( min(var1) - max(var2), min(var2) - max(var1) ))
             if(diff > mindiff):
-                result.append(( labels[c1], labels[c2], var_names[v], expression[c1], expression[c2], var1, var2 ))
-                print(result[-1])
-    if(result):
-        print()
+                score = get_score(expression[c1], expression[c2], diff)
+                normal.append(( labels[c1], labels[c2], var_names[v], expression[c1], expression[c2], var1, var2, score))
+    if(spurious):
+        #print(max(spurious, key = lambda x: x[-1]))
+        return [], max(spurious, key = lambda x: x[-1])
+    elif(normal):
+        return max(normal, key = lambda x: x[-1]), []
+    else:
+        return [], []
                 
                 
 
@@ -56,7 +76,36 @@ def parse_file(path):
         return labels, var_names, expression, variants
         
         
-
+normal_list, spurious_list = [], []
 for path in [x for x in get_only_files(args.path) if x.endswith("tsv")]:
+    name = os.path.basename(path).split(".")[0]
     labels, var_names, expression, variants = parse_file(path);
-    check(labels, var_names, expression, variants , args.minexpr, args.mindiff)
+    res =  check(labels, var_names, expression, variants , args.minexpr, args.mindiff, args.minfraction)
+    if(res[0]):
+        normal_list.append((name, res[0]))
+    if(res[1]):
+        spurious_list.append((name, res[1]))
+        
+        
+normal_list.sort(reverse=True, key = lambda x: x[1][-1])
+spurious_list.sort(reverse=True, key = lambda x: x[1][-1])        
+with open(os.path.join(args.outdir, "normal.tsv"), 'w') as f:
+    header = ["gene", "time1", "time2", "tss", "score", "expression1", "expression2", "fraction1", "fraction2"]
+    f.write("%s\n" % "\t".join(header) )
+    for name, l in normal_list:
+        a = [",".join([str(y) for y in x]) for x in l[3:7]]
+        f.write("%s\n" % "\t".join([name] + list(l[:3]) + [str(int((l[-1])))] + a) )
+        
+        
+with open(os.path.join(args.outdir, "spurious.tsv"), 'w') as f:
+    header = ["gene", "time", "tss", "score", "expression", "fraction"]
+    f.write("%s\n" % "\t".join(header) )
+    for name, l in spurious_list:
+        a = [",".join([str(y) for y in x]) for x in l[2:4]]
+        f.write("%s\n" % "\t".join([name] + list(l[:2]) + [str(int((l[-1])))] + a) )
+        
+        
+        
+        
+        
+        
--- mapping/coverage2bedgraph.py
+++ mapping/coverage2bedgraph.py
@@ -8,53 +8,93 @@ import sys
 
 from pybedtools import BedTool
 from collections import Counter
+import numpy as np
+
+from afbio.sequencetools import coverage2dict;
 
 parser = argparse.ArgumentParser(description='Converts coverage-per-position file into bedgrpah file');
-parser.add_argument('path', metavar = 'N', nargs = '?', type = str, help = "Path to the coverage file");
+parser.add_argument('path', metavar = 'N', nargs = '+', type = str, help = "Path to the coverage file");
 parser.add_argument('--trackopts', nargs = '?', default='', type = str, help = "Header for the (ucsc) track");
 parser.add_argument('--multiplier', nargs = '?', default=1.0, type = float, help = "Coverage multiplier, should be used only for the normalized tracks");
 parser.add_argument('--convert', nargs = '?', default=False, const=True, type = bool, help = "If set, converts chromosome names to human ones");
+parser.add_argument('--normalize', nargs = '?', default=False, const=True, type = bool, help = "If set, coverage is normalized to the mean value");
 #parser.add_argument('--plot', nargs = '?', type = str, help = "Output destination for the statistics plots");
 args = parser.parse_args();
 
+
+def flush(start, end, val, chr_count):
+    print("\t".join( [str(x) for x in ('chr%d' % chr_count, start, end, val)] ))
+
+
+def convert_coverage(coverage, chr_count):
+    #print(len(coverage))
+    curstart = 0;
+    curval = coverage[0];
+
+    for pos, val in enumerate(coverage[1:], start=1):
+        if(val != curval):
+            #print(val, curval, curstart)
+            flush(curstart, pos, curval, chr_count)
+            curstart = pos;
+            curval = val;
+    else:
+        flush(curstart, pos, val, chr_count)
+            
+            
+
+
+
 if(args.trackopts):
     print(args.trackopts);
-
-
-def flush(start, end, ident, convert, chr_count):
-    if(ident and ident[1]):
-        if(convert):
-            print("\t".join( [str(x) for x in ('chr%d' % chr_count, start, end, ident[1])] ))
-        else:
-            print("\t".join( [str(x) for x in (ident[0], start, end, ident[1])] ))
-
     
-curstart = 0;
-curident = None;
-chr_count = 2;
-cur_chrom = None
-chrom_ident = None
-chrom_end = None
+    
 
-with open(args.path) as f:
-    for l in f:
-        a = l.strip().split("\t");
-        start = int(a[1]);
-        ident = (a[0], round(float(a[2])*args.multiplier));
-        if(cur_chrom and  a[0] != cur_chrom):
-            flush(curstart, chrom_end, chrom_ident, args.convert, chr_count)
-            chr_count -= 1;
-            curstart = start;
-            curident = ident
+coverage_dict_list = [coverage2dict(x) for x in args.path];
+coverage_dict = {};
+for chrom in coverage_dict_list[0].keys():
+    temp = sum([x[chrom] for x in coverage_dict_list])
+    if(args.normalize):
+        temp = temp/np.mean(temp)
+    coverage_dict[chrom] = [int(x) for x in temp*args.multiplier]
+    
+coverage_dict = [x[1] for x in sorted(coverage_dict.items(), key = lambda x: x[0])]
+for chr_count, coverage in enumerate(coverage_dict, start=1):
+    convert_coverage(coverage, chr_count)
+    
 
-        elif(ident != curident):
-            flush(curstart, start, curident, args.convert, chr_count)
-            curstart = start;
-            curident = ident;
-        chrom_ident = ident;
-        chrom_end = start+1
-        cur_chrom = a[0]
-    else:
-        flush(curstart, start, curident, args.convert, chr_count)
+#sys.exit();
+
+
+
+
+
+
+
+#curstart = 0;
+#curident = None;
+#cur_chrom = None
+#chrom_ident = None
+#chrom_end = None
+
+#with open(args.path) as f:
+    #for l in f:
+        #a = l.strip().split("\t");
+        #start = int(a[1]);
+        #ident = (a[0], round(float(a[2])*args.multiplier));
+        #if(cur_chrom and  a[0] != cur_chrom):
+            #flush(curstart, chrom_end, chrom_ident, args.convert, chr_count)
+            #chr_count -= 1;
+            #curstart = start;
+            #curident = ident
+
+        #elif(ident != curident):
+            #flush(curstart, start, curident, args.convert, chr_count)
+            #curstart = start;
+            #curident = ident;
+        #chrom_ident = ident;
+        #chrom_end = start+1
+        #cur_chrom = a[0]
+    #else:
+        #flush(curstart, start, curident, args.convert, chr_count)
             
     
